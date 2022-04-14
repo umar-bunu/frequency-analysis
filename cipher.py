607a9fae568f68e7a87c3be9519c3d7e7c67d611
umar-bunu <umar.bunu73@gmail.com>
2022-04-14 14:41:23 +0300

    some few changes

--- cipher.py
+++ cipher.py
@@ -1,6 +1,10 @@
 import sys
 
 
+def sortAnalysis(letter):
+    return letter.get('freq')
+
+
 def showTxt(txt, typeTxt, shouldAnalyze):
     if(shouldAnalyze):
         print('\n---------BEGIN---------\n')
@@ -10,11 +14,23 @@ def showTxt(txt, typeTxt, shouldAnalyze):
         letters = [{'letter': 'a', 'freq': 0}, {'letter': 'b', 'freq': 0}, {'letter': 'c', 'freq': 0}, {'letter': 'd', 'freq': 0}, {'letter': 'e', 'freq': 0}, {'letter': 'f', 'freq': 0}, {'letter': 'g', 'freq': 0}, {'letter': 'h', 'freq': 0}, {'letter': 'i', 'freq': 0}, {'letter': 'j', 'freq': 0}, {'letter': 'k', 'freq': 0}, {'letter': 'l', 'freq': 0}, {'letter': 'm', 'freq': 0}, {
             'letter': 'n', 'freq': 0}, {'letter': 'o', 'freq': 0}, {'letter': 'p', 'freq': 0}, {'letter': 'q', 'freq': 0}, {'letter': 'r', 'freq': 0}, {'letter': 's', 'freq': 0}, {'letter': 't', 'freq': 0}, {'letter': 'u', 'freq': 0}, {'letter': 'v', 'freq': 0}, {'letter': 'w', 'freq': 0}, {'letter': 'x', 'freq': 0}, {'letter': 'y', 'freq': 0}, {'letter': 'z', 'freq': 0}]
         print('\nAnalysis:')
-        for eachLetter in letters:
-            letters[letters.index(eachLetter)]['freq'] = wholeTxt.count(
+
+        newlist = letters
+
+        for eachLetter in newlist:
+            newlist[newlist.index(eachLetter)]['freq'] = wholeTxt.count(
+                eachLetter['letter'].upper())
+
+        for i in range(len(newlist)):
+            for j in range(i, len(newlist)):
+                if newlist[j]['freq'] > newlist[i]['freq']:
+                    newlist[i], newlist[j] = newlist[j], newlist[i]
+
+        for eachLetter in newlist:
+            newlist[newlist.index(eachLetter)]['freq'] = wholeTxt.count(
                 eachLetter['letter'].upper())
             print(eachLetter['letter'].upper(
-            )+'->'+str(letters[letters.index(eachLetter)]['freq'])+', ', end="")
+            )+'->'+str(newlist[newlist.index(eachLetter)]['freq'])+', ', end="")
 
 
 def takeReplacement(wholeTxt, wholeTxtTemp2):
